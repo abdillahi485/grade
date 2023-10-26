6b1824299b155a4d90d5311138f7375975bddb46
Abdillahi485 <daakircadde143@gmail.com>
2023-10-26 06:16:17 +0300

    testing exam result

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hello");
+let grade=59;
+if (grade >=90 && grade <=100){
+    console.log("Your grade is A")
+}else if(grade>=75 && grade <90){
+    console.log("Your grade is B")
+}else if(grade>=60 && grade<75){
+    console.log("Your grade is C")
+}else  if(grade<60){
+    console.log("You are failed")
+}
